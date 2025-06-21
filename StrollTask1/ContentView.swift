eb3b6dc80bf539d9a27c4a2d20e7671a1ee9d3af
endrioo <linendrio@gmail.com>
2025-06-21 09:00:17 +0700

    Initial Commit

---
+++ StrollTask1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StrollTask1
+//
+//  Created by Endrio on 21/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StrollTask1/StrollTask1App.swift
@@ -0,0 +1,17 @@
+//
+//  StrollTask1App.swift
+//  StrollTask1
+//
+//  Created by Endrio on 21/06/25.
+//
+
+import SwiftUI
+
+@main
+struct StrollTask1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
